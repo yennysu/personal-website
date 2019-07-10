b8fdf80b9d9ec5889d14b66728beedf1e9dc80cf
yennysu <yennyysu@gmail.com>
2019-07-09 23:18:49 -0400

    loaded in SVG!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import LandscapeSVG from './components/LandscapeSVG'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <LandscapeSVG/>
         <p>
           This is Yenny's website.
         </p>
