7acf997b0dea6e3f52373d79a1f139ced03d1171
yennysu <yennyysu@gmail.com>
2019-07-11 10:50:14 -0400

    reorged landing page, added router imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './style.css';
 import LandscapeSVG from './components/LandscapeSVG'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import { Box, Grid, Typography, Paper, CardContent, Container}  from '@material-ui/core/';
 
 class App extends React.Component {
 
+  headline() {
+    return (
+      <Typography variant="h2" color='primary' align="center">
+        <Box letterSpacing={6}>
+        yenny su
+        </Box>
+      </Typography>
+    )
+  }
+
+  landscape() {
+    const inputs = {
+      style: {
+        margin: '3%'
+      }
+    }
+    return(
+      <div {...inputs}>
+        <LandscapeSVG/>
+      </div>
+    )
+  }
+
   render() {
-    const inputs ={
+    const inputs = {
       mainDiv: {
         style: {
           margin: '5%'
@@ -21,14 +45,8 @@ class App extends React.Component {
 
     return (
       <div {...inputs.mainDiv}>
-          <LandscapeSVG/>
-          <div {...inputs.textDiv}>
-            <Typography variant="h2" color='primary' align="center">
-              <Box letterSpacing={6}>
-              yenny su
-              </Box>
-            </Typography>
-          </div>
+        {this.headline()}
+        {this.landscape()}
       </div>
     );
   }
